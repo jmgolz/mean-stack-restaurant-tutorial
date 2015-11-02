16eaacd4d0302e90159ac0b106514bc7d1aa75d8
James Golz <jmgolz@gmail.com>
2015-11-02 02:17:44 +0000

    finished chapter 4 of the lynda tutorials. Had to hack in some different ways of structuring the API "data" object since my fake data does not match the tutorial

--- public/js/app/menu/menu.js
+++ public/js/app/menu/menu.js
@@ -3,27 +3,64 @@
    
    angular.module('app').controller('MenuController', MenuController);
    
-   MenuController.$inject = ['api', '$routeParams', 'ngDialog', '$scope'];
+   MenuController.$inject = ['api', '$routeParams', 'ngDialog', '$scope', '$location'];
    
-   function MenuController(api, $routeParams, ngDialog, $scope){
+   function MenuController(api, $routeParams, ngDialog, $scope, $location){
        var vm = this;
+       
+       vm.items = [];
        
        api.getRestaurantDetails($routeParams.restaurantId).then(function(data){
            vm.restaurant = data;
        });
        
        vm.viewItem = function(item){
-          vm.activeItem = item;
-          
-          //Disabled because in this tutorial, we are only
-          //using a text string, the incoming item is not an object.
-          //vm.activeItem.options = [];
-          
+          //vm.activeItem = item;
+          vm.activeItem = {
+              name: item,
+              options: ['test','test2','test3','test4'],
+              selectedItems: []
+          };
+
           ngDialog.open({
              template: 'item.html',
              className: 'ngdialog-theme-default',
              scope: $scope
           });
+          
+          vm.toggleOption = function(option){
+            var index = vm.activeItem.selectedItems.indexOf(option);
+            if(index > -1){
+                vm.activeItem.selectedItems.splice(index, 1);
+                return;
+            }
+            vm.activeItem.selectedItems.push(option);
+          };
+          
+          vm.addItem = function(item){
+              var newItem = { 
+                  item: item.name,
+                  options: []
+              };
+              
+              if(item.selectedItems.length > 0){
+                  item.selectedItems.map(function(item){
+                      newItem.options.push(item);
+                  });
+              }
+              
+              vm.items.push(newItem);
+              ngDialog.close();
+              console.log(vm.items);
+          };
+          
+          vm.cancel = function(){
+              ngDialog.close();
+          };
+          
+          vm.checkout = function(){
+              $location.url('/payment');
+          };
        }
    }
 })();
